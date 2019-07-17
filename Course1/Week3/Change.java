1204be7319577fa2526238cfdb04f87a17791ae6
henry <henry.byrd@claricode.com>
2019-07-16 20:35:19 -0400

    Finished change problem in java

--- Course1/Week3/Change.java
+++ Course1/Week3/Change.java
@@ -2,15 +2,44 @@ import java.util.Scanner;
 
 public class Change {
     private static int getChange(int m) {
-        //write your code here
-        return m;
+        int numCoins = 0;
+        int[] denominations = { 10, 5, 1 };
+        for (int i = 0; i < 3; i++) {
+            int coin = denominations[i];
+            if (m >= coin) {
+                int coinsUsed = m / coin;
+                numCoins += coinsUsed;
+                m -= coinsUsed * coin;
+            }
+        }
+        return numCoins;
+    }
+
+    private static void runSolution() {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        scanner.close();
+        System.out.println(getChange(m));
+    }
+
+    private static void testSolution() {
+        runTest(2, 2);
+        runTest(28, 6);
+        runTest(30, 3);
+        runTest(10, 1);
+        runTest(5, 1);
+        runTest(1, 1);
+    }
+
+    private static void runTest(int input, int expected) {
+        int result = getChange(input);
+        if (result != expected)
+            System.out.println("wrong change for " + input + ": " + result);
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int m = scanner.nextInt();
-        System.out.println(getChange(m));
-
+        testSolution();
+        // runSolution();
     }
 }
 
