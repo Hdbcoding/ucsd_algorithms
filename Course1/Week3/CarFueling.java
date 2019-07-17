28cf834bd910b17172c3dd666e00739294924a06
henry <henry.byrd@claricode.com>
2019-07-16 22:51:48 -0400

    Set up car fueling test cases

--- Course1/Week3/CarFueling.java
+++ Course1/Week3/CarFueling.java
@@ -1,12 +1,13 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class CarFueling {
     static int computeMinRefills(int dist, int tank, int[] stops) {
+
+
         return -1;
     }
 
-    public static void main(String[] args) {
+    private static void runSolution() {
         Scanner scanner = new Scanner(System.in);
         int dist = scanner.nextInt();
         int tank = scanner.nextInt();
@@ -15,7 +16,26 @@ public class CarFueling {
         for (int i = 0; i < n; i++) {
             stops[i] = scanner.nextInt();
         }
-
+        scanner.close();
         System.out.println(computeMinRefills(dist, tank, stops));
+    }
+
+    private static void testSolution() {
+        runTest(950, 400, new int[] { 200, 375, 550, 750 }, 2);
+        runTest(1650, 400, new int[] { 400, 800, 1200, 1600 }, 4);
+        runTest(1600, 400, new int[] { 400, 800, 1200, 1600 }, 3);
+        runTest(10, 3, new int[] { 1, 2, 5, 9 }, -1);
+        runTest(200, 250, new int[] { 100, 150 }, 0);
+    }
+    
+    private static void runTest(int dist, int tank, int[] stops, int expected) {
+        int actual = computeMinRefills(dist, tank, stops);
+        if (actual != expected)
+            System.out.println("Incorrect value for computeMinRefills, expected " + expected + ", but got " + actual);
+    }
+
+    public static void main(String[] args) {
+        testSolution();
+        // runSolution();
     }
 }
