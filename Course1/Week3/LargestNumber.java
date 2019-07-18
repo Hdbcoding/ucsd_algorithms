f904c7eb18866238868052e422184c6bf1b0db80
henry <henry.byrd@claricode.com>
2019-07-18 00:27:10 -0400

    Added a test case for largest number

--- Course1/Week3/LargestNumber.java
+++ Course1/Week3/LargestNumber.java
@@ -8,16 +8,17 @@ public class LargestNumber {
             public int compare(String o1, String o2) {
                 boolean o1Longer = o1.length() > o2.length();
                 int length = o1Longer ? o2.length() : o1.length();
-                for (int i = 0; i < length; i++){
+                for (int i = 0; i < length; i++) {
                     if (o1.charAt(i) > o2.charAt(i))
                         return 1;
                     if (o1.charAt(i) < o2.charAt(i))
                         return -1;
                 }
+                // "1" should be ">" than "10"
                 return 0;
             }
         });
-        for (int i = a.length - 1; i>= 0; i--) {
+        for (int i = a.length - 1; i >= 0; i--) {
             result += a[i];
         }
         return result;
@@ -40,6 +41,13 @@ public class LargestNumber {
         runTest(new String[] { "9", "4", "6", "1", "9" }, "99641");
         runTest(new String[] { "23", "39", "92" }, "923923");
         runTest(new String[] { "99", "901", "909" }, "99909901");
+        runTest(new String[] { "2", "8", "2", "3", "6", "4", "1", "1", "10", "6", "3", "3", "6", "1", "3", "8", "4",
+                "6", "1", "10", "8", "4", "10", "4", "1", "3", "2", "3", "2", "6", "1", "5", "2", "9", "8", "5", "10",
+                "8", "7", "9", "6", "4", "2", "6", "3", "8", "8", "9", "8", "2", "9", "10", "3", "10", "7", "5", "7",
+                "1", "7", "5", "1", "4", "7", "6", "1", "10", "5", "4", "8", "4", "2", "7", "8", "1", "1", "7", "4",
+                "1", "1", "9", "8", "6", "5", "9", "9", "3", "7", "6", "3", "10", "8", "10", "7", "2", "5", "1", "1",
+                "9", "9", "5" },
+                "9999999998888888888887777777776666666666555555554444444443333333333222222222111111111111111101010101010101010");
     }
 
     private static void runTest(String[] a, String expected) {
@@ -49,8 +57,7 @@ public class LargestNumber {
     }
 
     public static void main(String[] args) {
-        // testSolution();
-        runSolution();
+        testSolution();
+        // runSolution();
     }
 }
-
