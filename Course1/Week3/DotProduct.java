3daf49ae5e18dace6123c7b70f06f7f719a41829
henry <henry.byrd@claricode.com>
2019-07-17 20:50:32 -0400

    Implemented a solution to the dot product problem

--- Course1/Week3/DotProduct.java
+++ Course1/Week3/DotProduct.java
@@ -4,13 +4,15 @@ public class DotProduct {
     private static long maxDotProduct(int[] a, int[] b) {
         //write your code here
         long result = 0;
-        for (int i = 0; i < a.length; i++) {
-            result += a[i] * b[i];
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for (int i = a.length-1; i >= 0; i--) {
+            result += (long)a[i] * b[i];
         }
         return result;
     }
 
-    public static void main(String[] args) {
+    private static void runSolution() {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] a = new int[n];
@@ -21,7 +23,24 @@ public class DotProduct {
         for (int i = 0; i < n; i++) {
             b[i] = scanner.nextInt();
         }
+        scanner.close();
         System.out.println(maxDotProduct(a, b));
+    }
+    
+    private static void testSolution() {
+        runTest(new int[] { 23 }, new int[] { 39 }, 897);
+        runTest(new int[] { 1, 3, -5 }, new int[] { -2, 4, 1 }, 23);
+    }
+    
+    private static void runTest(int[] a, int[] b, long expected) {
+        long actual = maxDotProduct(a, b);
+        if (actual != expected)
+            System.out.println("Incorrect value for dot product, expected " + expected + ", but got " + actual);
+    }
+
+    public static void main(String[] args) {
+        testSolution();
+        // runSolution();
     }
 }
 
