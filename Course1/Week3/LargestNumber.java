723f3a9b12e4df317e0e32e3f05afc22e537be8e
henry <henry.byrd@claricode.com>
2019-07-18 00:11:09 -0400

    Implemented the largest number problem

--- Course1/Week3/LargestNumber.java
+++ Course1/Week3/LargestNumber.java
@@ -2,22 +2,55 @@ import java.util.*;
 
 public class LargestNumber {
     private static String largestNumber(String[] a) {
-        //write your code here
         String result = "";
-        for (int i = 0; i < a.length; i++) {
+        Arrays.sort(a, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                boolean o1Longer = o1.length() > o2.length();
+                int length = o1Longer ? o2.length() : o1.length();
+                for (int i = 0; i < length; i++){
+                    if (o1.charAt(i) > o2.charAt(i))
+                        return 1;
+                    if (o1.charAt(i) < o2.charAt(i))
+                        return -1;
+                }
+                return 0;
+            }
+        });
+        for (int i = a.length - 1; i>= 0; i--) {
             result += a[i];
         }
         return result;
     }
 
-    public static void main(String[] args) {
+    private static void runSolution() {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         String[] a = new String[n];
         for (int i = 0; i < n; i++) {
             a[i] = scanner.next();
         }
+        scanner.close();
         System.out.println(largestNumber(a));
+    }
+
+    private static void testSolution() {
+        runTest(new String[] { "21", "22" }, "2221");
+        runTest(new String[] { "21", "2" }, "221");
+        runTest(new String[] { "9", "4", "6", "1", "9" }, "99641");
+        runTest(new String[] { "23", "39", "92" }, "923923");
+        runTest(new String[] { "99", "901", "909" }, "99909901");
+    }
+
+    private static void runTest(String[] a, String expected) {
+        String actual = largestNumber(a);
+        if (!actual.equals(expected))
+            System.out.println("Incorrect value for largestNumber, expected " + expected + ", but got " + actual);
+    }
+
+    public static void main(String[] args) {
+        // testSolution();
+        runSolution();
     }
 }
 
