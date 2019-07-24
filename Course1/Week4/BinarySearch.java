72f38047fcbfa3f98c60e4351b81d35ebc795943
henry <henry.byrd@claricode.com>
2019-07-23 21:35:33 -0400

    Implemented binary search and majority element

--- Course1/Week4/BinarySearch.java
+++ Course1/Week4/BinarySearch.java
@@ -4,20 +4,26 @@ import java.util.*;
 public class BinarySearch {
 
     static int binarySearch(int[] a, int x) {
-        int left = 0, right = a.length;
-        //write your code here
-
+        int left = 0, right = a.length - 1, middle, value;
+        while (left <= right) {
+            middle = (left + right) / 2;
+            value = a[middle];
+            if (value == x) return middle;
+            else if (value < x) left = middle + 1;
+            else right = middle - 1;
+        }
         return -1;
     }
 
     static int linearSearch(int[] a, int x) {
         for (int i = 0; i < a.length; i++) {
-            if (a[i] == x) return i;
+            if (a[i] == x)
+                return i;
         }
         return -1;
     }
-
-    public static void main(String[] args) {
+    
+    static void runSolution() {
         FastScanner scanner = new FastScanner(System.in);
         int n = scanner.nextInt();
         int[] a = new int[n];
@@ -27,13 +33,35 @@ public class BinarySearch {
         int m = scanner.nextInt();
         int[] b = new int[m];
         for (int i = 0; i < m; i++) {
-          b[i] = scanner.nextInt();
+            b[i] = scanner.nextInt();
         }
         for (int i = 0; i < m; i++) {
             //replace with the call to binarySearch when implemented
-            System.out.print(linearSearch(a, b[i]) + " ");
+            System.out.print(binarySearch(a, b[i]) + " ");
         }
     }
+    
+    static void testSolution() {
+        runTest(new int[] { 1, 5, 8, 12, 13 }, 8, 2);
+        runTest(new int[] { 1, 5, 8, 12, 13 }, 1, 0);
+        runTest(new int[] { 1, 5, 8, 12, 13 }, 23, -1);
+        runTest(new int[] { 1, 5, 8, 12, 13 }, 11, -1);
+        runTest(new int[] { 1, 5, 8, 12, 13 }, 6, -1);
+        runTest(new int[] { 1, 5, 8, 12, 13 }, 5, 1);
+        runTest(new int[] { 1, 5, 8, 12, 13 }, 13, 4);
+    }
+    
+    static void runTest(int[] a, int x, int expected) {
+        int actual = binarySearch(a, x);
+        if (actual != expected)
+            System.out.println("Expected " + expected + ", but result was " + actual);
+    }
+
+    public static void main(String[] args) {
+        // testSolution();
+        runSolution();
+    }
+    
     static class FastScanner {
         BufferedReader br;
         StringTokenizer st;
--- Course1/Week4/MajorityElement.java
+++ Course1/Week4/MajorityElement.java
@@ -2,30 +2,61 @@ import java.util.*;
 import java.io.*;
 
 public class MajorityElement {
-    private static int getMajorityElement(int[] a, int left, int right) {
-        if (left == right) {
-            return -1;
+    static int getMajorityElement(int[] a) {
+        // find an element that passes the majority smell test
+        int index = 0, count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[index] == a[i])
+                count++;
+            else
+                count--;
+            if (count == 0) {
+                index = i;
+                count = 1;
+            }
         }
-        if (left + 1 == right) {
-            return a[left];
+        // check that this element actually is the majority
+        int majorityElement = a[index];
+        int majoritySize = a.length / 2;
+        count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == majorityElement && ++count > majoritySize )
+                return majorityElement;
         }
-        //write your code here
         return -1;
     }
 
-    public static void main(String[] args) {
+    static void runSolution() {
         FastScanner scanner = new FastScanner(System.in);
         int n = scanner.nextInt();
         int[] a = new int[n];
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-        if (getMajorityElement(a, 0, a.length) != -1) {
+        if (getMajorityElement(a) != -1) {
             System.out.println(1);
         } else {
             System.out.println(0);
         }
     }
+    
+    static void testSolution() {
+        runTest(new int[] { 2, 3, 9, 2, 2 }, 2);
+        runTest(new int[] { 1, 2, 3, 4 }, -1);
+        runTest(new int[] { 1, 2, 3, 1 }, -1);
+    }
+    
+    static void runTest(int[] a, int expected) {
+        int actual = getMajorityElement(a);
+        if (actual != expected)
+            System.out.println("Expected " + expected + ", but result was " + actual);
+    }
+
+    public static void main(String[] args) {
+        testSolution();
+        //runSolution();
+    }
+
     static class FastScanner {
         BufferedReader br;
         StringTokenizer st;
