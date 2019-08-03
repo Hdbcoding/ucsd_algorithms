3c2eb4f763e1805a0156a3807ae5be9d6451b81f
henry <henry.byrd@claricode.com>
2019-08-03 10:18:08 -0400

    Implemented a solution to the change problem

--- Course1/Week5/ChangeDP.java
+++ Course1/Week5/ChangeDP.java
@@ -1,16 +1,46 @@
 import java.util.Scanner;
 
 public class ChangeDP {
-    private static int getChange(int m) {
-        //write your code here
-        return m / 4;
+    //note, denominations are 4, 3, 1
+    static int getChange(int m) {
+        int[] change = new int[Math.max(m + 1, 5)];
+        change[0] = 0;
+        change[1] = 1;
+        change[2] = 2;
+        change[3] = 1;
+        change[4] = 1;
+
+        for (int i = 5; i <= m; i++) {
+            change[i] = Math.min(change[i - 4] + 1, Math.min(change[i - 3] + 1, change[i - 1] + 1));
+        }
+
+        return change[m];
+    }
+
+    static void runSolution() {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        scanner.close();
+        System.out.println(getChange(m));
+    }
+
+    static void testSolution() {
+        runTest(2, 2);
+        runTest(6, 2);
+        runTest(7, 2);
+        runTest(8, 2);
+        runTest(9, 3);
+        runTest(34, 9);
+    }
+    
+    static void runTest(int m, int expected) {
+        int actual = getChange(m);
+        if (actual != expected) System.out.println("Incorrect change calculated for " + m 
+            + ", expected: " + expected + ", but got: " + actual);
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int m = scanner.nextInt();
-        System.out.println(getChange(m));
-
+        // testSolution();
+        runSolution();
     }
 }
-
