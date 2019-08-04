ddc7f8db51e58c702573831cedb01b3d101fd49b
henry <henry.byrd@claricode.com>
2019-08-03 21:10:38 -0400

    implemented lcs2

--- Course1/Week5/LCS2.java
+++ Course1/Week5/LCS2.java
@@ -1,13 +1,47 @@
 import java.util.*;
 
 public class LCS2 {
+    static int lcs2(int[] a, int[] b) {
+        int[][] distances = new int[a.length + 1][b.length + 1];
 
-    private static int lcs2(int[] a, int[] b) {
-        //Write your code here
-        return Math.min(a.length, b.length);
+        for (int i = 1; i <= a.length; i++) {
+            int ai = a[i - 1];
+            for (int j = 1; j <= b.length; j++) {
+                int bj = b[j - 1];
+                int i1j1 = distances[i - 1][j - 1] + (ai == bj ? 1 : 0);
+                int i1j0 = distances[i - 1][j];
+                int i0j1 = distances[i][j - 1];
+                distances[i][j] = Math.max(i1j1, Math.max(i1j0, i0j1));
+            }
+        }
+
+        return distances[a.length][b.length];
     }
 
     public static void main(String[] args) {
+        // testSolution();
+        runSolution();
+    }
+
+    static void testSolution() {
+        runTest(new int[] { 2, 7, 5 }, new int[] { 2, 5 }, 2);
+        runTest(new int[] { 7 }, new int[] { 1, 2, 3, 4 }, 0);
+        runTest(new int[] { 2, 7, 8, 3 }, new int[] { 5, 2, 8, 7 }, 2);
+        runTest(new int[] { 2, 3, 1, 2, 3 }, new int[] { 1, 2, 3 }, 3);
+        runTest(new int[] { 2, 2, 2, 3 }, new int[] { 2, 3 }, 2);
+        runTest(new int[] { 2, 7, 7, 7, 5 }, new int[] { 2, 7, 7, 5 }, 4);
+        runTest(new int[] { 3, 3, 1 }, new int[] { 1, 3, 3 }, 2);
+        runTest(new int[] { 2, 7, 5, 2 }, new int[] { 2, 5 }, 2);
+    }
+
+    static void runTest(int[] a, int[] b, int expected) {
+        int actual = lcs2(a, b);
+        if (actual != expected)
+            System.out.println("Wrong length of longest subsequence between " + Arrays.toString(a) + ", and "
+                    + Arrays.toString(b) + ". Expected " + expected + ", but got " + actual);
+    }
+
+    static void runSolution() {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] a = new int[n];
@@ -20,8 +54,8 @@ public class LCS2 {
         for (int i = 0; i < m; i++) {
             b[i] = scanner.nextInt();
         }
+        scanner.close();
 
         System.out.println(lcs2(a, b));
     }
 }
-
--- Course1/Week5/LCS3.java
+++ Course1/Week5/LCS3.java
@@ -1,13 +1,30 @@
 import java.util.*;
 
 public class LCS3 {
-
-    private static int lcs3(int[] a, int[] b, int[] c) {
-        //Write your code here
+    static int lcs3(int[] a, int[] b, int[] c) {
+        // Write your code here
         return Math.min(Math.min(a.length, b.length), c.length);
     }
 
     public static void main(String[] args) {
+        testSolution();
+        // runSolution();
+    }
+
+    static void testSolution() {
+        runTest(new int[] { 1, 2, 3 }, new int[] { 2, 1, 3 }, new int[] { 1, 3, 5 }, 2);
+        runTest(new int[] { 8, 3, 2, 1, 7 }, new int[] { 8, 2, 1, 3, 8, 10, 7 }, new int[] { 6, 8, 3, 1, 4, 7 }, 3);
+    }
+
+    static void runTest(int[] a, int[] b, int[] c, int expected) {
+        int actual = lcs3(a, b, c);
+        if (actual != expected)
+            System.out.println(
+                    "Wrong length of longest subsequence between " + Arrays.toString(a) + ", " + Arrays.toString(b)
+                            + ", and " + Arrays.toString(c) + ". Expected " + expected + ", but got " + actual);
+    }
+
+    static void runSolution() {
         Scanner scanner = new Scanner(System.in);
         int an = scanner.nextInt();
         int[] a = new int[an];
@@ -24,7 +41,7 @@ public class LCS3 {
         for (int i = 0; i < cn; i++) {
             c[i] = scanner.nextInt();
         }
+        scanner.close();
         System.out.println(lcs3(a, b, c));
     }
 }
-
