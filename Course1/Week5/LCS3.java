acfacb700bcf8ffe74bb82745fe06dc9511b83ba
henry <henry.byrd@claricode.com>
2019-08-03 21:56:25 -0400

    Implemented a solution to the lcs 3 problem

--- Course1/Week5/LCS3.java
+++ Course1/Week5/LCS3.java
@@ -2,18 +2,50 @@ import java.util.*;
 
 public class LCS3 {
     static int lcs3(int[] a, int[] b, int[] c) {
-        // Write your code here
-        return Math.min(Math.min(a.length, b.length), c.length);
+        int[][][] distances = new int[a.length + 1][b.length + 1][c.length + 1];
+        for (int i = 1; i <= a.length; i++) {
+            int ai = a[i - 1];
+            for (int j = 1; j <= b.length; j++) {
+                int bj = b[j - 1];
+                for (int k = 1; k <= c.length; k++) {
+                    int ck = c[k - 1];
+                    boolean allEqual = ai == bj && bj == ck;
+                    int[] previousValues = getAllPrevious(distances, i, j, k, allEqual);
+                    distances[i][j][k] = maxValue(previousValues);
+                }
+            }
+        }
+        return distances[a.length][b.length][c.length];
+    }
+
+    private static int[] getAllPrevious(int[][][] distances, int i, int j, int k, boolean allEqual) {
+        return new int[] {
+                distances[i - 1][j - 1][k - 1] + (allEqual ? 1 : 0),
+                distances[i - 1][j - 1][k],
+                distances[i - 1][j]    [k - 1],
+                distances[i]    [j - 1][k - 1],
+                distances[i]    [j]    [k - 1],
+                distances[i]    [j - 1][k],
+                distances[i - 1][j]    [k]
+        };
+    }
+
+    private static int maxValue(int[] previousValues) {
+        int max = 0;
+        for (int i : previousValues)
+            max = Math.max(i, max);
+        return max;
     }
 
     public static void main(String[] args) {
-        testSolution();
-        // runSolution();
+        // testSolution();
+        runSolution();
     }
 
     static void testSolution() {
         runTest(new int[] { 1, 2, 3 }, new int[] { 2, 1, 3 }, new int[] { 1, 3, 5 }, 2);
         runTest(new int[] { 8, 3, 2, 1, 7 }, new int[] { 8, 2, 1, 3, 8, 10, 7 }, new int[] { 6, 8, 3, 1, 4, 7 }, 3);
+
     }
 
     static void runTest(int[] a, int[] b, int[] c, int expected) {
