e8573b4bd4bbea0619b4c872dcb6f20934613b74
henry <henry.byrd@claricode.com>
2019-08-24 19:59:21 -0400

    Added an easy memoization solution

--- Course2/Week1/tree_height.java
+++ Course2/Week1/tree_height.java
@@ -46,7 +46,8 @@ public class tree_height {
 		return root;
 	}
 
-	static int computeHeight(int n, int[] parents) {
+	// something is wrong with either the traversal or the tree construction, leading to infinite loops
+	static int computeHeightByGeneratingTree(int n, int[] parents) {
 		Node root = generateTree(n, parents);
 		int maxHeight = 0;
 		Queue<Node> q = new LinkedList<Node>();
@@ -65,26 +66,62 @@ public class tree_height {
 					q.add(child);
 			}
 		}
+	}
 
-		// // Replace this code with a faster implementation
-		// for (int vertex = 0; vertex < n; vertex++) {
-		// int height = 0;
-		// for (int i = vertex; i != -1; i = parent[i])
-		// height++;
-		// maxHeight = Math.max(maxHeight, height);
-		// }
-		// return maxHeight;
+	static int computeHeightMemo(int n, int[] parents) {
+		int max = 0;
+		int[] heights = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			if (heights[i] != 0)
+				continue;
+			// traverse up the parents of this node, until we find the root. 
+			// Use the traversal to calculate the height of this node
+			int height = 0;
+			for (int p = i; p != -1; p = parents[p]) {
+				// if we have seen an ancestor, stop traversing
+				if (heights[p] != 0) {
+					height += heights[p];
+					break;
+				}
+				height++;
+			}
+			max = Math.max(max, height);
+
+			// to save time later, fill in any parents with missing heights
+			for (int p = i; p != -1; p = parents[p]) {
+				if (heights[p] != 0)
+					break;
+				heights[p] = height--;
+			}
+		}
+		return max;
+	}
+
+	static int computeHeightNaive(int n, int[] parents) {
+		int maxHeight = 0;
+		for (int vertex = 0; vertex < n; vertex++) {
+			int height = 0;
+			for (int i = vertex; i != -1; i = parents[i])
+				height++;
+			maxHeight = Math.max(maxHeight, height);
+		}
+		return maxHeight;
 	}
 
 	static public void main(String[] args) throws IOException {
-		new tree_height().testSolution();
-		// runSolution();
+		// new tree_height().testSolution();
+		runSolution();
 	}
 
 	void testSolution() {
 		runTest(5, new int[] { -1, 0, 4, 0, 3 }, 4);
 		runTest(5, new int[] { 4, -1, 4, 1, 1 }, 3);
 		runTest(10, new int[] { 9, 7, 5, 5, 2, 9, 9, 9, 2, -1 }, 4);
+		runTest(100, new int[] { 96, 61, 95, 34, 12, 26, 48, 42, 69, 74, 90, 67, 8, 53, 65, 0, 14, 47, 88, 8, 49, 4, 93,
+				75, 6, 29, -1, 62, 87, 12, 42, 52, 1, 46, 4, 83, 14, 75, 72, 95, 15, 86, 29, 53, 85, 78, 65, 31, 5, 96,
+				6, 74, 87, 24, 15, 90, 22, 85, 20, 46, 78, 97, 50, 97, 69, 19, 21, 61, 92, 5, 22, 47, 63, 1, 93, 28, 20,
+				34, 52, 21, 72, 88, 67, 0, 86, 49, 63, 48, 28, 25, 50, 83, 31, 19, 62, 24, 64, 64, 92, 25 }, 8);
 		runTest(100, new int[] { 59, 30, 0, 1, 80, 51, 83, 25, 40, 35, 59, 77, 22, 31, 47, 41, 56, 36, 68, 9, 89, 6, 72,
 				44, 5, 54, 23, 63, 70, 87, 90, 22, 8, 18, 71, 92, 97, 29, 48, 97, 14, 62, 55, 89, 15, 15, 82, 55, 88, 2,
 				19, 36, 0, 91, 64, 81, 69, 11, 60, 56, 34, 77, 98, 70, 96, 20, 50, 57, 8, 4, 13, 93, 65, 57, 53, 3, 20,
@@ -92,7 +129,7 @@ public class tree_height {
 	}
 
 	void runTest(int n, int[] parents, int expected) {
-		int actual = computeHeight(n, parents);
+		int actual = computeHeightMemo(n, parents);
 		if (actual != expected)
 			System.out.println("Incorrect height for " + Arrays.toString(parents) + ", expected: " + expected
 					+ ", but got " + actual);
@@ -105,7 +142,7 @@ public class tree_height {
 		for (int i = 0; i < n; i++) {
 			parent[i] = in.nextInt();
 		}
-		System.out.println(computeHeight(n, parent));
+		System.out.println(computeHeightMemo(n, parent));
 	}
 
 	static class FastScanner {
