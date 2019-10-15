b52872ed97373cfea930698a38784cb51cb05bca
henry <henry.byrd@claricode.com>
2019-10-14 21:43:03 -0400

    Reorganized max sliding window a little

--- Course2/Week1/max_sliding_window.java
+++ Course2/Week1/max_sliding_window.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 
 public class max_sliding_window {
-    static void max_sliding_window_naive(int[] A, int w) {
+    static int[] max_sliding_window_naive(int[] A, int w) {
+        int[] solutions = new int[A.length - w + 1];
+        int k = 0;
         for (int i = 0; i < A.length; i++) {
             int max = A[i];
             for (int j = i + 1; j < i + w; j++)
                 max = Math.max(max, A[j]);
-            System.out.println(max);
+            solutions[k++] = max;
         }
+        return solutions;
     }
 
     public static void main(String[] args) {
+        runSolution();
+    }
+
+    static void runSolution() {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] A = new int[n];
@@ -18,6 +25,8 @@ public class max_sliding_window {
             A[i] = scanner.nextInt();
         int w = scanner.nextInt();
         scanner.close();
-        max_sliding_window_naive(A, w);
+        int[] solutions = max_sliding_window_naive(A, w);
+        for (int s : solutions)
+            System.out.println(s);
     }
 }
