bfbcd18bd8c4d52604f1f28d476012dda7503e5a
henry <hdbyrd1@gmail.com>
2019-11-17 12:00:31 -0500

    Completed BuildHeap for local test cases

--- Course2/Week2/BuildHeap.java
+++ Course2/Week2/BuildHeap.java
@@ -1,38 +1,60 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class BuildHeap {
-  private int[] data;
-  private List<Swap> swaps;
-
-  private FastScanner in;
-  private PrintWriter out;
-
   public static void main(String[] args) throws IOException {
-    new BuildHeap().solve();
+    // runSolution();
+    testSolution();
   }
 
-  public void solve() throws IOException {
-    in = new FastScanner();
-    out = new PrintWriter(new BufferedOutputStream(System.out));
-    readData();
-    generateSwaps();
-    writeResponse();
-    out.close();
+  public static void runSolution() throws IOException {
+    int[] data = readData();
+    List<Swap> swaps = generateSwapsNaive(data);
+    writeResponse(swaps);
   }
 
-  private void readData() throws IOException {
-    int n = in.nextInt();
-    data = new int[n];
-    for (int i = 0; i < n; ++i) {
-      data[i] = in.nextInt();
+  public static void testSolution(){
+    runTest(new int[]{5, 4, 3, 2, 1}, new int[]{3, 1, 4, 0, 1, 1, 3});
+    runTest(new int[]{1, 2, 3, 4, 5}, new int[]{0});
+  }
+
+  public static void runTest(int[] data, int[] expected) {
+    List<Swap> actualSwaps = generateSwapsFast(data);
+    int[] actual = generateResponse(actualSwaps);
+    String actualString = Arrays.toString(actual);
+    String expectedString = Arrays.toString(expected);
+    if (!actualString.equals(expectedString)) {
+      System.out.println("Unexpected result for " + Arrays.toString(data) + ", expected: " + expectedString
+          + ", but got: " + actualString);
     }
   }
 
-  private void generateSwaps() {
-    swaps = new ArrayList<Swap>();
+  private static int[] generateResponse(List<Swap> actualSwaps) {
+    int[] actual = new int[actualSwaps.size() * 2 + 1];
+    actual[0] = actualSwaps.size();
+    int i = 1;
+    for (int j = 0; j < actualSwaps.size(); j++) {
+      actual[i++] = actualSwaps.get(j).index1;
+      actual[i++] = actualSwaps.get(j).index2;
+    }
+    return actual;
+  }
+
+  private static int[] readData() throws IOException {
+    FastScanner in = new FastScanner();
+    int n = in.nextInt();
+    int[] data = new int[n];
+    for (int i = 0; i < n; ++i) {
+      data[i] = in.nextInt();
+    }
+    return data;
+  }
+
+  private static List<Swap> generateSwapsNaive(int[] data) {
+    List<Swap> swaps = new ArrayList<Swap>();
     // The following naive implementation just sorts
     // the given sequence using selection sort algorithm
     // and saves the resulting sequence of swaps.
@@ -50,33 +72,50 @@ public class BuildHeap {
         }
       }
     }
+    return swaps;
   }
 
-  private void writeResponse() {
+  private static List<Swap> generateSwapsFast(int[] data){
+    Heap heap = new Heap(data);
+    List<Swap> swaps = heap.heapify();
+    return swaps;
+  }
+
+  private static void writeResponse(List<Swap> swaps) {
+    PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
     out.println(swaps.size());
     for (Swap swap : swaps) {
       out.println(swap.index1 + " " + swap.index2);
     }
+    out.close();
   }
 
   static class Heap {
     int[] _data;
 
-    Heap(int[] data) {
+    private Heap(int[] data) {
       this._data = data;
     }
 
-    public void siftDown(int i, ArrayList<Swap> swaps) {
+    public List<Swap> heapify(){
+      List<Swap> swaps = new ArrayList<Swap>();
+      for (int i = _data.length / 2; i >= 0; i--){
+        siftDown(i, swaps);
+      }
+      return swaps;
+    }
+
+    public void siftDown(int i, List<Swap> swaps) {
       // if we have no children, we can't sit down
       int l = Left(i), r = Right(i), s = size(), swapIndex = i;
 
-      if (l <= s && !rule(swapIndex, l))
+      if (l < s && !rule(swapIndex, l))
         swapIndex = l;
-      
-      if (r <= s && !rule(swapIndex, r))
+
+      if (r < s && !rule(swapIndex, r))
         swapIndex = r;
-      
-      if (swapIndex != i){
+
+      if (swapIndex != i) {
         Swap(i, swapIndex);
         swaps.add(new Swap(i, swapIndex));
         siftDown(swapIndex, swaps);
@@ -117,6 +156,11 @@ public class BuildHeap {
     public Swap(int index1, int index2) {
       this.index1 = index1;
       this.index2 = index2;
+    }
+
+    @Override
+    public String toString(){
+      return index1 + " " + index2;
     }
   }
 
