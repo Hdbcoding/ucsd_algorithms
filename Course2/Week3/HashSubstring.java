34b937992d93369944bcd6993fa6a4310d85f82f
henry <hdbyrd1@gmail.com>
2019-11-23 17:29:13 -0500

    reformatted hashSubstring

--- Course2/Week3/HashSubstring.java
+++ Course2/Week3/HashSubstring.java
@@ -1,31 +1,33 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class HashSubstring {
-
-    private static FastScanner in;
-    private static PrintWriter out;
-
     public static void main(String[] args) throws IOException {
-        in = new FastScanner();
-        out = new PrintWriter(new BufferedOutputStream(System.out));
-        printOccurrences(getOccurrences(readInput()));
+        runSolution();
+        testSolution();
+    }
+
+    static void runSolution() throws IOException {
+        FastScanner in = new FastScanner();
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+        printOccurrences(getOccurrences(readInput(in)), out);
         out.close();
     }
 
-    private static Data readInput() throws IOException {
-        String pattern = in.next();
-        String text = in.next();
-        return new Data(pattern, text);
+    static void testSolution() {
+
     }
 
-    private static void printOccurrences(List<Integer> ans) throws IOException {
-        for (Integer cur : ans) {
-            out.print(cur);
-            out.print(" ");
-        }
+    static void runTest(Data input, List<Integer> expected) {
+        List<Integer> actual = getOccurrences(input);
+        String eString = Arrays.toString(expected.toArray());
+        String aString = Arrays.toString(actual.toArray());
+        if (!eString.equals(aString))
+            System.out.println("Unexpected result for " + input.text + ", " + input.pattern + ". Expected " + eString
+                    + ", but got " + aString);
     }
 
     private static List<Integer> getOccurrences(Data input) {
@@ -33,22 +35,36 @@ public class HashSubstring {
         int m = s.length(), n = t.length();
         List<Integer> occurrences = new ArrayList<Integer>();
         for (int i = 0; i + m <= n; ++i) {
-	    boolean equal = true;
-	    for (int j = 0; j < m; ++j) {
-		if (s.charAt(j) != t.charAt(i + j)) {
-		     equal = false;
- 		    break;
-		}
-	    }
+            boolean equal = true;
+            for (int j = 0; j < m; ++j) {
+                if (s.charAt(j) != t.charAt(i + j)) {
+                    equal = false;
+                    break;
+                }
+            }
             if (equal)
                 occurrences.add(i);
-	}
+        }
         return occurrences;
+    }
+
+    private static Data readInput(FastScanner in) throws IOException {
+        String pattern = in.next();
+        String text = in.next();
+        return new Data(pattern, text);
+    }
+
+    private static void printOccurrences(List<Integer> ans, PrintWriter out) throws IOException {
+        for (Integer cur : ans) {
+            out.print(cur);
+            out.print(" ");
+        }
     }
 
     static class Data {
         String pattern;
         String text;
+
         public Data(String pattern, String text) {
             this.pattern = pattern;
             this.text = text;
@@ -76,4 +92,3 @@ public class HashSubstring {
         }
     }
 }
-
