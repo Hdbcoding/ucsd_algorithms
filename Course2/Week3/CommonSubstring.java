9220d05aaeb75dc37903205ea4c6aac8f332bc81
henry <hdbyrd1@gmail.com>
2019-11-23 11:49:16 -0500

    reformatted CommonSubstring problem

--- Course2/Week3/CommonSubstring.java
+++ Course2/Week3/CommonSubstring.java
@@ -2,26 +2,12 @@ import java.util.*;
 import java.io.*;
 
 public class CommonSubstring {
-    public class Answer {
-        int i, j, len;
-        Answer(int i, int j, int len) {
-            this.i = i;
-            this.j = j;
-            this.len = len;
-        }
+    static public void main(String[] args) {
+        // runSolution();
+        testSolution();
     }
 
-    public Answer solve(String s, String t) {
-        Answer ans = new Answer(0, 0, 0);
-        for (int i = 0; i < s.length(); i++)
-            for (int j = 0; j < t.length(); j++)
-                for (int len = 0; i + len <= s.length() && j + len <= t.length(); len++)
-                    if (len > ans.len && s.substring(i, i + len).equals(t.substring(j, j + len)))
-                        ans = new Answer(i, j, len);
-        return ans;
-    }
-
-    public void run() {
+    static void runSolution(){
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(System.out);
         in.lines().forEach(line -> {
@@ -34,7 +20,39 @@ public class CommonSubstring {
         out.close();
     }
 
-    static public void main(String[] args) {
-        new CommonSubstring().run();
+    static void testSolution(){
+
+    }
+
+    static void runTest(String s, String t, Answer expected){
+        Answer actual = solve(s, t);
+        if (!expected.equals(actual))
+            System.out.println("Unexpected result for " + s + ", " + t);
+    }
+
+    static Answer solve(String s, String t) {
+        Answer ans = new Answer(0, 0, 0);
+        for (int i = 0; i < s.length(); i++)
+            for (int j = 0; j < t.length(); j++)
+                for (int len = 0; i + len <= s.length() && j + len <= t.length(); len++)
+                    if (len > ans.len && s.substring(i, i + len).equals(t.substring(j, j + len)))
+                        ans = new Answer(i, j, len);
+        return ans;
+    }
+    
+    static class Answer {
+        int i, j, len;
+        Answer(int i, int j, int len) {
+            this.i = i;
+            this.j = j;
+            this.len = len;
+        }
+
+        @Override
+        public boolean equals(Object other){
+            if (!(other instanceof Answer)) return false;
+            Answer o = (Answer)other;
+            return this.i == o.i && this.j == o.j && this.len == o.len;
+        }
     }
 }
