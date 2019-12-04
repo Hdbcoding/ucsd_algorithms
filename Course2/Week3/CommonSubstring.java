4d634f95184ee8ec9027d4a8f306154776b1944e
henry <hdbyrd1@gmail.com>
2019-12-03 19:34:26 -0500

    Implemented the hashset version of the substring finder

--- Course2/Week3/CommonSubstring.java
+++ Course2/Week3/CommonSubstring.java
@@ -3,15 +3,15 @@ import java.io.*;
 
 public class CommonSubstring {
     static public void main(String[] args) {
-        // runSolution();
+        runSolution();
         // testSolution();
-        runStressTest();
+        // runStressTest();
     }
 
     static void runSolution() {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(System.out);
-        CommonSubstringFinder finder = new NaiveSubstringFinder();
+        CommonSubstringFinder finder = new HashingSubstringFinder();
         in.lines().forEach(line -> {
             StringTokenizer tok = new StringTokenizer(line);
             String s = tok.nextToken();
@@ -27,10 +27,12 @@ public class CommonSubstring {
         runTest("cool", "toolbox", new Answer(1, 1, 3));
         runTest("aaa", "bb", new Answer(0, 1, 0));
         runTest("aabaa", "babbaab", new Answer(0, 4, 3));
+        runTest("aabaababaacc", "cc", new Answer(10, 0, 2));
+        runTest("cc", "aabaababaacc", new Answer(0, 10, 2));
     }
 
     static void runTest(String s, String t, Answer expected) {
-        CommonSubstringFinder finder = new BinarySubstringFinder();
+        CommonSubstringFinder finder = new HashingSubstringFinder();
         finder.initialize(s, t);
         Answer actual = finder.solve();
         complainAboutDiscrepancy(s, t, expected, actual);
@@ -48,22 +50,22 @@ public class CommonSubstring {
     }
 
     static void runStressTest() {
-        CommonSubstringFinder naive = new NaiveSubstringFinder();
-        CommonSubstringFinder fast = new BinarySubstringFinder();
-        for (int i = 0; i < 500; i++) {
-            if ((i % 10) == 0){
+        CommonSubstringFinder naive = new BinarySubstringFinder();
+        CommonSubstringFinder fast = new HashingSubstringFinder();
+        for (int i = 0; i < 1000; i++) {
+            if ((i % 10) == 0) {
                 System.out.println(i);
             }
             String s = getRandomString();
             String t = getRandomString();
 
-            naive.initialize(s, t);
+            // naive.initialize(s, t);
             fast.initialize(s, t);
 
-            Answer expected = naive.solve();
+            // Answer expected = naive.solve();
             Answer actual = fast.solve();
 
-            complainAboutDiscrepancy(s, t, expected, actual);
+            // complainAboutDiscrepancy(s, t, expected, actual);
         }
     }
 
@@ -74,37 +76,253 @@ public class CommonSubstring {
         int targetStringLength = random.nextInt(500) + 1;
         StringBuilder buffer = new StringBuilder(targetStringLength);
         for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int) 
-            (random.nextFloat() * (rightLimit - leftLimit + 1));
+            int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
             buffer.append((char) randomLimitedInt);
         }
         return buffer.toString();
     }
 
+    static int x = 31;
+    static int p1 = 1000000007;
+    static int p2 = 1000000009;
+
     static class HashingSubstringFinder implements CommonSubstringFinder {
         String s;
         String t;
-        int x = 31;
-        int p1 = 1000000007;
-        int p2 = 1000000009;
-        int[] s_p1Hashes;
-        int[] s_p2Hashes;
-        int[] t_p1Hashes;
-        int[] t_p2Hashes;
+
         int[] pow_x1;
         int[] pow_x2;
 
+        HashHelper s_helper;
+        HashHelper t_helper;
+
         @Override
         public void initialize(String s, String t) {
-            // TODO Auto-generated method stub
+            this.s = s;
+            s_helper = new HashHelper(s);
+            s_helper.initialize();
+
+            this.t = t;
+            t_helper = new HashHelper(t);
+            t_helper.initialize();
+
+            buildPowersOfX();
+        }
+
+        void buildPowersOfX() {
+            int l = Math.max(s_helper.l, t_helper.l);
+            if (pow_x1 == null)
+                buildPowersOfXFromScratch(l);
+            else if (pow_x1.length < l)
+                expandPowersOfX(l);
+        }
+
+        void buildPowersOfXFromScratch(int l) {
+            pow_x1 = new int[l];
+            pow_x2 = new int[l];
+            int y1 = 1;
+            int y2 = 1;
+            for (int i = 0; i < l; i++) {
+                y1 = setPowX(y1, p1, pow_x1, i);
+                y2 = setPowX(y2, p2, pow_x2, i);
+            }
+        }
+
+        void expandPowersOfX(int l) {
+            int[] pow_x1_new = new int[l];
+            int[] pow_x2_new = new int[l];
+            int i = 0;
+            for (; i < pow_x1.length; i++) {
+                pow_x1_new[i] = pow_x1[i];
+                pow_x2_new[i] = pow_x2[i];
+            }
+            int y1 = pow_x1[i - 1];
+            int y2 = pow_x2[i - 1];
+            for (; i < l; i++) {
+                y1 = setPowX(y1, p1, pow_x1_new, i);
+                y2 = setPowX(y2, p2, pow_x2_new, i);
+            }
+            pow_x1 = pow_x1_new;
+            pow_x2 = pow_x2_new;
         }
 
         @Override
         public Answer solve() {
-            // TODO Auto-generated method stub
+            Answer ans = new Answer(0, 0, 0);
+            int shortest = 1;
+            int longest = Math.min(s.length(), t.length());
+
+            while (shortest <= longest) {
+                int m = (shortest + longest) / 2;
+                Answer best = findSubstring(m);
+                if (best != null) {
+                    ans = best;
+                    shortest = m + 1;
+                } else {
+                    longest = m - 1;
+                }
+            }
+            return ans;
+        }
+
+        Answer findSubstring(int l) {
+            int y1 = pow_x1[l - 1];
+            int y2 = pow_x2[l - 1];
+            DoubleHashSet set = buildHashSet(s_helper, l, y1, y2);
+            return findMatchingSubstring(set, t_helper, l, y1, y2);
+        }
+
+        DoubleHashSet buildHashSet(HashHelper helper, int l, int y1, int y2) {
+            int numHashes = helper.l - l + 1;
+            DoubleHashSet set = new DoubleHashSet(numHashes);
+
+            for (int i = 0; i < numHashes; i++) {
+                DoubleHash dh = getDoubleHash(helper, y1, y2, i, l);
+                set.add(dh.h1, dh.h2, i, l);
+            }
+
+            return set;
+        }
+
+        DoubleHash getDoubleHash(HashHelper helper, int y1, int y2, int i, int l) {
+            int h1 = getHashCode(helper.p1Hashes, p1, y1, i, l);
+            int h2 = getHashCode(helper.p2Hashes, p2, y2, i, l);
+            return new DoubleHash(h1, h2);
+        }
+
+        int getHashCode(int[] hashes, int p, int y, int i, int l) {
+            long v = hashes[i + l - 1] - (long) y * (i == 0 ? 0 : hashes[i - 1]);
+            return safeModulo(v, p);
+        }
+
+        private Answer findMatchingSubstring(DoubleHashSet set, HashHelper helper, int l, int y1, int y2) {
+            int numHashes = helper.l - l + 1;
+            for (int j = 0; j < numHashes; j++) {
+                DoubleHash dh = getDoubleHash(helper, y1, y2, j, l);
+                HashRecord r = set.get(dh.h1, dh.h2);
+                if (r != null)
+                    return new Answer(r.i, j, l);
+            }
             return null;
         }
 
+        class HashHelper {
+            String s;
+            int l;
+            int[] p1Hashes;
+            int[] p2Hashes;
+
+            HashHelper(String s) {
+                this.s = s;
+                l = s.length();
+                p1Hashes = new int[l];
+                p2Hashes = new int[l];
+            }
+
+            void initialize() {
+                p1Hashes[0] = s.charAt(0);
+                p2Hashes[0] = s.charAt(0);
+
+                for (int i = 1; i < l; i++) {
+                    char c = s.charAt(i);
+                    setPrefixHash(p1, p1Hashes, i, c);
+                    setPrefixHash(p2, p2Hashes, i, c);
+                }
+            }
+
+            void setPrefixHash(int p, int[] hashes, int i, char c) {
+                long v = (long) x * hashes[i - 1] + c;
+                hashes[i] = safeModulo(v, p);
+            }
+        }
+
+        class DoubleHash {
+            int h1;
+            int h2;
+
+            DoubleHash(int h1, int h2) {
+                this.h1 = h1;
+                this.h2 = h2;
+            }
+        }
+
+        class DoubleHashSet {
+            ArrayList<LinkedList<HashRecord>> set1;
+            ArrayList<LinkedList<HashRecord>> set2;
+            int bucketCount;
+
+            DoubleHashSet(int bucketCount) {
+                this.bucketCount = bucketCount;
+                this.set1 = new ArrayList<LinkedList<HashRecord>>(bucketCount);
+                this.set2 = new ArrayList<LinkedList<HashRecord>>(bucketCount);
+                for (int i = 0; i < bucketCount; i++) {
+                    set1.add(new LinkedList<HashRecord>());
+                    set2.add(new LinkedList<HashRecord>());
+                }
+            }
+
+            void add(int h1, int h2, int i, int l) {
+                addToBucket(set1, h1, i, l);
+                addToBucket(set2, h2, i, l);
+            }
+
+            HashRecord get(int h1, int h2) {
+                LinkedList<HashRecord> l1 = getBucket(set1, h1);
+                LinkedList<HashRecord> l2 = getBucket(set2, h2);
+
+                HashRecord r1 = getFirst(l1, h1);
+                HashRecord r2 = getFirst(l2, h2);
+                if (r1 == null || r2 == null)
+                    return null;
+                if (r1.i != r2.i)
+                    return null;
+                return r1;
+            }
+
+            private HashRecord getFirst(LinkedList<HashRecord> l, int h) {
+                for (HashRecord r : l)
+                    if (r.hash == h)
+                        return r;
+                return null;
+            }
+
+            void addToBucket(ArrayList<LinkedList<HashRecord>> set, int h, int i, int l) {
+                LinkedList<HashRecord> l1 = getBucket(set, h);
+                if (!listContains(l1, h))
+                    l1.addFirst(new HashRecord(h, i, l));
+            }
+
+            private boolean listContains(LinkedList<HashRecord> l1, int h) {
+                return l1.stream().anyMatch(r -> r.hash == h);
+            }
+
+            LinkedList<HashRecord> getBucket(ArrayList<LinkedList<HashRecord>> set, int hash) {
+                return set.get(hash % bucketCount);
+            }
+        }
+
+        class HashRecord {
+            int hash;
+            int i;
+            int l;
+
+            HashRecord(int hash, int i, int l) {
+                this.hash = hash;
+                this.i = i;
+                this.l = l;
+            }
+        }
+    }
+
+    static int safeModulo(long v, int p) {
+        return (int) ((v % p) + p) % p;
+    }
+
+    static int setPowX(int y, int p, int[] pow_x, int i) {
+        long v = (long) y * x;
+        y = safeModulo(v, p);
+        pow_x[i] = y;
+        return y;
     }
 
     static class BinarySubstringFinder implements CommonSubstringFinder {
@@ -136,7 +354,7 @@ public class CommonSubstring {
             return ans;
         }
 
-        private Answer findSubstring(int len) {
+        Answer findSubstring(int len) {
             for (int i = 0; i < s.length() - len + 1; i++) {
                 for (int j = 0; j < t.length() - len + 1; j++) {
                     if (validSolution(s, t, i, j, len))
@@ -175,7 +393,7 @@ public class CommonSubstring {
         Answer solve();
     }
 
-    private static boolean validSolution(String s, String t, Answer ans) {
+    static boolean validSolution(String s, String t, Answer ans) {
         return validSolution(s, t, ans.i, ans.j, ans.len);
     }
 
