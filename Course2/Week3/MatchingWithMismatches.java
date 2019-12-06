3af9dc73b60bcf1854097fa8b2b2f1e64482a470
henry <hdbyrd1@gmail.com>
2019-12-06 13:33:40 -0500

    Implemented the naive solution to the matching with mismatches problem

--- Course2/Week3/MatchingWithMismatches.java
+++ Course2/Week3/MatchingWithMismatches.java
@@ -4,19 +4,21 @@ import java.io.*;
 
 public class MatchingWithMismatches {
     static public void main(String[] args) {
-        runSolution();
+        // runSolution();
         testSolution();
     }
 
     static void runSolution() {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(System.out);
+        MismatchMathcer matcher = new NaiveMismatchMatcher();
         in.lines().forEach(line -> {
             StringTokenizer tok = new StringTokenizer(line);
             int k = Integer.valueOf(tok.nextToken());
             String s = tok.nextToken();
             String t = tok.nextToken();
-            List<Integer> ans = solve(k, s, t);
+            matcher.initialize(s, t);
+            List<Integer> ans = matcher.solve(k);
             out.format("%d ", ans.size());
             out.println(ans.stream().map(n -> String.valueOf(n)).collect(Collectors.joining(" ")));
         });
@@ -24,13 +26,61 @@ public class MatchingWithMismatches {
     }
 
     static void testSolution() {
+        runTest(0, "ababab", "baaa", new int[] { });
+        runTest(1, "ababab", "baaa", new int[] { 1 });
+        runTest(1, "xabcabc", "ccc", new int[] { });
+        runTest(2, "xabcabc", "ccc", new int[] { 1, 2, 3, 4 });
+        runTest(3, "aaa", "xxx", new int[] { 0 });
     }
 
-    static void runTest() {
+    static void runTest(int k, String s, String t, int[] expected) {
+        MismatchMathcer matcher = new NaiveMismatchMatcher();
+        matcher.initialize(s, t);
+        List<Integer> actual = matcher.solve(k);
+        String expectedString = Arrays.toString(expected);
+        String actualString = Arrays.toString(actual.toArray());
+        if (!expectedString.equals(actualString))
+            System.out.println("Unexpected result for " + k + ", " + s + ", " + t + ". Expected " + expectedString
+                    + ", but got " + actualString);
+
     }
 
-    static List<Integer> solve(int k, String text, String pattern) {
-        ArrayList<Integer> pos = new ArrayList<>();
-        return pos;
+    static class NaiveMismatchMatcher implements MismatchMathcer {
+        String s;
+        String t;
+
+        @Override
+        public void initialize(String s, String t) {
+            this.s = s;
+            this.t = t;
+        }
+
+        @Override
+        public List<Integer> solve(int k) {
+            List<Integer> matches = new ArrayList<>();
+            for (int i = 0; i <= s.length() - t.length(); i++) {
+                if (isMatch(k, i))
+                    matches.add(i);
+            }
+            return matches;
+        }
+
+        private boolean isMatch(int k, int i) {
+            int misMatches = 0;
+            for (int j = 0; j < t.length(); j++) {
+                if (t.charAt(j) != s.charAt(i + j))
+                    ++misMatches;
+                if (misMatches > k)
+                    return false;
+            }
+            return true;
+        }
+
+    }
+
+    interface MismatchMathcer {
+        void initialize(String s, String t);
+
+        List<Integer> solve(int k);
     }
 }
