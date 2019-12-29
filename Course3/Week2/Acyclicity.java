dee46f0ecf1f0da624e2884f7b221e3ebec45fd3
henry <hdbyrd1@gmail.com>
2019-12-29 12:28:37 -0500

    Finished first problem on detecting cycles

--- Course3/Week2/Acyclicity.java
+++ Course3/Week2/Acyclicity.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Acyclicity {
     public static void main(String[] args) {
-        // runSolution();
-        testSolution();
+        runSolution();
+        // testSolution();
     }
 
-    static void runSolution(){
+    static void runSolution() {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
@@ -36,15 +36,27 @@ public class Acyclicity {
         return adj;
     }
 
-    static void testSolution(){
-
+    static void testSolution() {
+        runTest(parseGraph(4, new int[] { 1, 2, 4, 1, 2, 3, 3, 1 }), 1);
+        runTest(parseGraph(5, new int[] { 1, 2, 2, 3, 1, 3, 3, 4, 1, 4, 2, 5, 3, 5
+        }), 0);
     }
 
-    static void runTest(ArrayList<Integer>[] adj, int expected){
+    static ArrayList<Integer>[] parseGraph(int nVertices, int[] edges) {
+        int x, y;
+        ArrayList<Integer>[] adj = constructGraph(nVertices);
+        for (int i = 0; i < edges.length - 1; i += 2) {
+            x = edges[i];
+            y = edges[i + 1];
+            addEdgeToAdjacencyList(adj, x, y);
+        }
+        return adj;
+    }
+
+    static void runTest(ArrayList<Integer>[] adj, int expected) {
         int actual = acyclic(adj);
         if (actual != expected)
-            System.out.println(
-                    "Unexpected result for " + printGraph(adj) + ". Expected: " + expected);
+            System.out.println("Unexpected result for " + printGraph(adj) + ". Expected: " + expected);
     }
 
     static String printGraph(ArrayList<Integer>[] adj) {
@@ -61,12 +73,51 @@ public class Acyclicity {
     }
 
     private static int acyclic(ArrayList<Integer>[] adj) {
-        //write your code here
-        return 0;
+        CyclicityChecker checker = new CyclicityChecker(adj);
+        return checker.hasCycles() ? 1 : 0;
     }
 
-    class CyclicityChecker {
+    static class CyclicityChecker {
+        ArrayList<Integer>[] adj;
+        boolean[] visited;
+        int[] post;
+        int postIndex = 0;
 
+        CyclicityChecker(ArrayList<Integer>[] adj) {
+            this.adj = adj;
+            visited = new boolean[adj.length];
+            post = new int[adj.length];
+        }
+
+        boolean hasCycles() {
+            dfs();
+            return verifyEdges();
+        }
+
+        void dfs() {
+            for (int i = 0; i < adj.length; i++)
+                if (!visited[i])
+                    explore(i);
+        }
+
+        private void explore(int i) {
+            visited[i] = true;
+            ArrayList<Integer> n = adj[i];
+            for (int j : n)
+                if (!visited[j])
+                    explore(j);
+            post[i] = postIndex++;
+        }
+
+        boolean verifyEdges() {
+            for (int i = 0; i < adj.length; i++) {
+                ArrayList<Integer> n = adj[i];
+                for (int j : n) {
+                    if (post[i] < post[j])
+                        return true;
+                }
+            }
+            return false;
+        }
     }
 }
-
