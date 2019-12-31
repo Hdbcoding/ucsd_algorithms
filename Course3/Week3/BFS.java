085c231444e838997ae8cb00e9ee8ca78fe95060
henry <hdbyrd1@gmail.com>
2019-12-31 12:52:20 -0500

    Finished the week 3 problems

--- Course3/Week3/BFS.java
+++ Course3/Week3/BFS.java
@@ -1,32 +1,120 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class BFS {
-    private static int distance(ArrayList<Integer>[] adj, int s, int t) {
-        //write your code here
-        return -1;
+    public static void main(String[] args) {
+        runSolution();
+        // testSolution();
     }
 
-    public static void main(String[] args) {
+    static void runSolution() {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
-        }
+        ArrayList<Integer>[] adj = constructGraph(n);
         for (int i = 0; i < m; i++) {
             int x, y;
             x = scanner.nextInt();
             y = scanner.nextInt();
-            adj[x - 1].add(y - 1);
-            adj[y - 1].add(x - 1);
+            addEdgeToAdjacencyList(adj, x, y);
         }
         int x = scanner.nextInt() - 1;
         int y = scanner.nextInt() - 1;
+        scanner.close();
         System.out.println(distance(adj, x, y));
     }
-}
 
+    static void testSolution() {
+        runTest(parseGraph(4, new int[] { 1, 2, 4, 1, 2, 3, 3, 1 }), 1, 3, 2);
+        runTest(parseGraph(5, new int[] { 5, 2, 1, 3, 3, 4, 1, 4 }), 2, 4, -1);
+    }
+
+    static void runTest(ArrayList<Integer>[] adj, int s, int t, int expected) {
+        int actual = distance(adj, s, t);
+        if (expected != actual)
+            System.out.println("Unexpected distance between nodes" + s + " and " + t + " for graph: " + printGraph(adj)
+                    + "Expected " + expected + " but got " + actual);
+    }
+
+    static ArrayList<Integer>[] constructGraph(int length) {
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[length];
+        for (int i = 0; i < length; i++) {
+            adj[i] = new ArrayList<Integer>();
+        }
+        return adj;
+    }
+
+    static void addEdgeToAdjacencyList(ArrayList<Integer>[] adj, int x, int y) {
+        adj[x - 1].add(y - 1);
+        adj[y - 1].add(x - 1);
+    }
+
+    static ArrayList<Integer>[] parseGraph(int nVertices, int[] edges) {
+        int x, y;
+        ArrayList<Integer>[] adj = constructGraph(nVertices);
+        for (int i = 0; i < edges.length - 1; i += 2) {
+            x = edges[i];
+            y = edges[i + 1];
+            addEdgeToAdjacencyList(adj, x, y);
+        }
+        return adj;
+    }
+
+    static String printGraph(ArrayList<Integer>[] adj) {
+        StringBuilder s = new StringBuilder();
+
+        for (int i = 0; i < adj.length; i++) {
+            ArrayList<Integer> edges = adj[i];
+            s.append("node " + i + ": ");
+            s.append(Arrays.toString(edges.toArray()));
+            s.append("\n");
+        }
+
+        return s.toString();
+    }
+
+    static int distance(ArrayList<Integer>[] adj, int s, int t) {
+        BFSSolver solver = new BFSSolver(adj);
+        return solver.distance(s, t);
+    }
+
+    static class BFSSolver {
+        ArrayList<Integer>[] adj;
+        int[] dist;
+
+        public BFSSolver(ArrayList<Integer>[] adj) {
+            this.adj = adj;
+            this.dist = new int[adj.length];
+            for (int i = 0; i < adj.length; i++)
+                dist[i] = -1;
+        }
+
+        int distance(int s, int t) {
+            if (s == t)
+                return 0;
+            dist[s] = 0;
+
+            Queue<Integer> q = new LinkedList<Integer>();
+            q.add(s);
+            int u;
+            int d;
+            while (!q.isEmpty()) {
+                u = q.poll();
+                d = dist[u];
+                for (int n : adj[u]) {
+                    if (dist[n] == -1) {
+                        dist[n] = d + 1;
+                        q.add(n);
+                        if (n == t)
+                            return dist[n];
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- Course3/Week3/Bipartite.java
+++ Course3/Week3/Bipartite.java
@@ -1,30 +1,131 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Bipartite {
-    private static int bipartite(ArrayList<Integer>[] adj) {
-        //write your code here
-        return -1;
+    public static void main(String[] args) {
+        runSolution();
+        // testSolution();
     }
 
-    public static void main(String[] args) {
+    static void runSolution() {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
-        }
+        ArrayList<Integer>[] adj = constructGraph(n);
         for (int i = 0; i < m; i++) {
             int x, y;
             x = scanner.nextInt();
             y = scanner.nextInt();
-            adj[x - 1].add(y - 1);
-            adj[y - 1].add(x - 1);
+            addEdgeToAdjacencyList(adj, x, y);
         }
+        scanner.close();
         System.out.println(bipartite(adj));
     }
-}
 
+    static void testSolution() {
+        runTest(parseGraph(4, new int[] { 1, 2, 4, 1, 2, 3, 3, 1 }), 0);
+        runTest(parseGraph(5, new int[] { 5, 2, 4, 2, 3, 4, 1, 4 }), 1);
+    }
+
+    static void runTest(ArrayList<Integer>[] adj, int expected) {
+        int actual = bipartite(adj);
+        if (expected != actual)
+            System.out.println(
+                    "Unexpected result for graph: " + printGraph(adj) + "Expected " + expected + " but got " + actual);
+    }
+
+    static ArrayList<Integer>[] constructGraph(int length) {
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[length];
+        for (int i = 0; i < length; i++) {
+            adj[i] = new ArrayList<Integer>();
+        }
+        return adj;
+    }
+
+    static void addEdgeToAdjacencyList(ArrayList<Integer>[] adj, int x, int y) {
+        adj[x - 1].add(y - 1);
+        adj[y - 1].add(x - 1);
+    }
+
+    static ArrayList<Integer>[] parseGraph(int nVertices, int[] edges) {
+        int x, y;
+        ArrayList<Integer>[] adj = constructGraph(nVertices);
+        for (int i = 0; i < edges.length - 1; i += 2) {
+            x = edges[i];
+            y = edges[i + 1];
+            addEdgeToAdjacencyList(adj, x, y);
+        }
+        return adj;
+    }
+
+    static String printGraph(ArrayList<Integer>[] adj) {
+        StringBuilder s = new StringBuilder();
+
+        for (int i = 0; i < adj.length; i++) {
+            ArrayList<Integer> edges = adj[i];
+            s.append("node " + i + ": ");
+            s.append(Arrays.toString(edges.toArray()));
+            s.append("\n");
+        }
+
+        return s.toString();
+    }
+
+    private static int bipartite(ArrayList<Integer>[] adj) {
+        BipartiteSolver solver = new BipartiteSolver(adj);
+        return solver.isBipartite() ? 1 : 0;
+    }
+
+    static class BipartiteSolver {
+        ArrayList<Integer>[] adj;
+        int[] dist;
+
+        public BipartiteSolver(ArrayList<Integer>[] adj) {
+            this.adj = adj;
+            this.dist = new int[adj.length];
+            for (int i = 0; i < adj.length; i++)
+                dist[i] = -1;
+        }
+
+        boolean isBipartite() {
+            for (int i = 0; i < adj.length; i++)
+                BFS(i);
+            for (int i = 0; i < adj.length; i++)
+                if (!isBipartite(i))
+                    return false;
+            return true;
+        }
+
+        private void BFS(int i) {
+            if (dist[i] != -1) return;
+            dist[i] = 0;
+            Queue<Integer> q = new LinkedList<Integer>();
+            q.add(i);
+            int u;
+            int d;
+            while (!q.isEmpty()) {
+                u = q.poll();
+                d = dist[u];
+                for (int n : adj[u]) {
+                    if (dist[n] == -1) {
+                        dist[n] = d + 1;
+                        q.add(n);
+                    }
+                }
+            }
+        }
+
+        private boolean isBipartite(int i) {
+            int m = dist[i] % 2;
+
+            for (int n : adj[i])
+                if (m == dist[n] % 2)
+                    return false;
+
+            return true;
+        }
+    }
+}
