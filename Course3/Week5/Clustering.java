9597bbc4e597407ac829955712c38bb67eec68e0
henry <hdbyrd1@gmail.com>
2020-01-13 22:18:20 -0500

    Refactored the week 5 problems

--- Course3/Week5/Clustering.java
+++ Course3/Week5/Clustering.java
@@ -1,22 +1,73 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Clustering {
-    private static double clustering(int[] x, int[] y, int k) {
-        //write your code here
+    public static void main(String[] args) {
+        // runSolution();
+        testSolution();
+    }
+
+    static void runSolution() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Graph g = new Graph(n);
+        for (int i = 0; i < n; i++) {
+            g.setPoint(i, scanner.nextInt(), scanner.nextInt());
+        }
+        scanner.close();
+        int k = scanner.nextInt();
+        System.out.println(clustering(g, k));
+    }
+
+    static void testSolution() {
+        runTest(new int[] { 12, 7, 6, 4, 3, 5, 1, 1, 7, 2, 7, 5, 7, 3, 3, 7, 8, 2, 8, 4, 4, 6, 7, 2, 6, 3 },
+                2.828427124746);
+        runTest(new int[] { 8, 3, 1, 1, 2, 4, 6, 9, 8, 9, 9, 8, 9, 3, 11, 4, 12, 4 }, 5);
+    }
+
+    static void runTest(int[] data, double expected) {
+        Graph g = parseGraph(data);
+        int k = data[data.length - 1];
+        double actual = clustering(g, k);
+        if (!closeEnough(expected, actual))
+            System.out.println("Unexpected result for x: " + Arrays.toString(g.x) + ", y: " + Arrays.toString(g.y)
+                    + ", expected: " + expected + ", but got: " + actual);
+    }
+
+    private static Graph parseGraph(int[] data) {
+        Graph g = new Graph(data[0]);
+        for (int i = 0; i < (data.length - 1) / 2; i++) {
+            int x = data[i * 2 + 1];
+            int y = data[i * 2 + 2];
+            g.setPoint(i, x, y);
+        }
+        return g;
+    }
+
+    private static boolean closeEnough(double expected, double actual) {
+        double epsilon = 1e-5;
+        return (actual > expected - epsilon) && (actual < expected + epsilon);
+    }
+
+    private static double clustering(Graph g, int k) {
+        // write your code here
         return -1.;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] x = new int[n];
-        int[] y = new int[n];
-        for (int i = 0; i < n; i++) {
-            x[i] = scanner.nextInt();
-            y[i] = scanner.nextInt();
+    static class Graph {
+        int[] x;
+        int[] y;
+        int s;
+
+        Graph(int s) {
+            this.s = s;
+            this.x = new int[s];
+            this.y = new int[s];
         }
-        int k = scanner.nextInt();
-        System.out.println(clustering(x, y, k));
+
+        public void setPoint(int i, int x, int y) {
+            this.x[i] = x;
+            this.y[i] = y;
+        }
     }
 }
-
--- Course3/Week5/ConnectingPoints.java
+++ Course3/Week5/ConnectingPoints.java
@@ -1,22 +1,71 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ConnectingPoints {
-    private static double minimumDistance(int[] x, int[] y) {
+    public static void main(String[] args) {
+        // runSolution();
+        testSolution();
+    }
+
+    static void runSolution() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Graph g = new Graph(n);
+        for (int i = 0; i < n; i++) {
+            g.setPoint(i, scanner.nextInt(), scanner.nextInt());
+        }
+        scanner.close();
+        System.out.println(minimumDistance(g));
+    }
+
+    static void testSolution() {
+        runTest(new int[] { 4, 0, 0, 0, 1, 1, 0, 1, 1 }, 3);
+        runTest(new int[] { 5, 0, 0, 0, 2, 1, 1, 3, 0, 3, 2 }, 7.064495102);
+    }
+
+    static void runTest(int[] data, double expected) {
+        Graph g = parseGraph(data);
+        double actual = minimumDistance(g);
+        if (!closeEnough(expected, actual))
+            System.out.println("Unexpected result for x: " + Arrays.toString(g.x) + ", y: " + Arrays.toString(g.y)
+                    + ", expected: " + expected + ", but got: " + actual);
+    }
+
+    private static Graph parseGraph(int[] data) {
+        Graph g = new Graph(data[0]);
+        for (int i = 0; i < (data.length - 1) / 2; i++) {
+            int x = data[i * 2 + 1];
+            int y = data[i * 2 + 2];
+            g.setPoint(i, x, y);
+        }
+        return g;
+    }
+
+    private static boolean closeEnough(double expected, double actual) {
+        double epsilon = 1e-5;
+        return (actual > expected - epsilon) && (actual < expected + epsilon);
+    }
+
+    static double minimumDistance(Graph g) {
         double result = 0.;
-        //write your code here
+        // write your code here
         return result;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] x = new int[n];
-        int[] y = new int[n];
-        for (int i = 0; i < n; i++) {
-            x[i] = scanner.nextInt();
-            y[i] = scanner.nextInt();
+    static class Graph {
+        int[] x;
+        int[] y;
+        int s;
+
+        Graph(int s) {
+            this.s = s;
+            this.x = new int[s];
+            this.y = new int[s];
         }
-        System.out.println(minimumDistance(x, y));
+
+        public void setPoint(int i, int x, int y) {
+            this.x[i] = x;
+            this.y[i] = y;
+        }
     }
 }
-
