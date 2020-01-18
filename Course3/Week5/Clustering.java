29417f069b1d1d81dfdcd8cf77e6f628d02f74a0
henry <hdbyrd1@gmail.com>
2020-01-18 17:53:12 -0500

    Implemented the clustering algorithm

--- Course3/Week5/Clustering.java
+++ Course3/Week5/Clustering.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Clustering {
@@ -50,8 +52,22 @@ public class Clustering {
     }
 
     private static double clustering(Graph g, int k) {
-        // write your code here
-        return -1.;
+        DisjointSet set = new DisjointSet(g.s);
+        ArrayList<Edge> edges = CalculateAllEdges(g);
+        int numClusters = g.s;
+        double minDistance = -1.;
+
+        for (Edge e : edges){
+            if (set.find(e.i) != set.find(e.j)){
+                if (numClusters == k){
+                    minDistance = e.cost;
+                    break;
+                }
+                set.merge(e.i, e.j);
+                numClusters--;
+            }
+        }
+        return minDistance;
     }
 
     static class Graph {
@@ -68,6 +84,73 @@ public class Clustering {
         public void setPoint(int i, int x, int y) {
             this.x[i] = x;
             this.y[i] = y;
+        }
+
+        double distance(int i, int j) {
+            int dx = x[i] - x[j];
+            int dy = y[i] - y[j];
+            return Math.sqrt(dx * dx + dy * dy);
+        }
+    }
+
+    static ArrayList<Edge> CalculateAllEdges(Graph g) {
+        ArrayList<Edge> edges = new ArrayList<Edge>();
+        for (int i = 0; i < g.s; i++) {
+            for (int j = i + 1; j < g.s; j++) {
+                Edge e = new Edge();
+                e.i = i;
+                e.j = j;
+                e.cost = g.distance(i, j);
+                edges.add(e);
+            }
+        }
+        Collections.sort(edges);
+        return edges;
+    }
+
+    static class DisjointSet {
+        int[] parent;
+        int[] rank;
+
+        DisjointSet(int s) {
+            parent = new int[s];
+            for (int i = 0; i < s; i++)
+                parent[i] = i;
+            rank = new int[s];
+        }
+
+        int find(int i) {
+            if (parent[i] != i)
+                parent[i] = find(parent[i]);
+            return parent[i];
+        }
+
+        void merge(int i, int j) {
+            i = find(i);
+            j = find(j);
+            if (i == j)
+                ;
+
+            if (rank[i] > rank[j])
+                parent[j] = i;
+            else {
+                parent[i] = j;
+                if (rank[i] == rank[j])
+                    rank[j]++;
+            }
+        }
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int i;
+        int j;
+        double cost;
+
+        @Override
+        public int compareTo(Edge o) {
+            if (cost < o.cost) return -1;
+            if (cost > o.cost) return 1;
+            return 0;
         }
     }
 }
