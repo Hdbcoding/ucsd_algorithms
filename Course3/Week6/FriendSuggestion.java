713a52b53b9012c1a67fa3aaaef74baf22b4205e
henry <hdbyrd1@gmail.com>
2020-01-20 22:41:28 -0500

    Building out testing procedure

--- Course3/Week6/FriendSuggestion.java
+++ Course3/Week6/FriendSuggestion.java
@@ -8,7 +8,7 @@ public class FriendSuggestion {
         runSolution();
     }
 
-    static void runSolution(){
+    static void runSolution() {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int m = in.nextInt();
@@ -28,9 +28,38 @@ public class FriendSuggestion {
             int u, v;
             u = in.nextInt();
             v = in.nextInt();
-            System.out.println(bd.query(u-1, v-1));
+            System.out.println(bd.query(u - 1, v - 1));
         }
         in.close();
+    }
+
+    void testSolution() {
+        runTest(new int[] {}, new long[] {});
+    }
+
+    private void runTest(int[] data, long[] expected) {
+        // todo - parse data to fill bd and q
+        int n = data[0];
+        int m = data[1];
+
+        BidirectionalDijkstra bd = new BidirectionalDijkstra(0);
+        ArrayList<Query> queries = new ArrayList<Query>();
+        long[] actual = new long[queries.size()];
+
+        for (int i = 0; i < queries.size(); i++) {
+            Query q = queries.get(i);
+            actual[i] = bd.query(q.u, q.v);
+        }
+
+        String eString = Arrays.toString(expected);
+        String aString = Arrays.toString(actual);
+        String qString = Arrays.toString(queries.toArray());
+
+        if (!eString.equals(aString)) {
+            System.out.println("Unexpected result for graph:\n" + bd.g + "\n and queries " + qString + ".\nExpected: "
+                    + eString + ", but got: " + aString);
+        }
+
     }
 
     static class BidirectionalDijkstra {
@@ -61,7 +90,7 @@ public class FriendSuggestion {
             g.visit(s, 0l);
             gr.visit(t, 0l);
             // Implement the rest of the algorithm yourself
- 
+
             return -1l;
         }
 
@@ -72,20 +101,20 @@ public class FriendSuggestion {
             Arrays.fill(visited, false);
         }
 
-        void visit (Graph g, int i, long distance){
+        void visit(Graph g, int i, long distance) {
             g.visit(i, distance);
             visited[i] = true;
         }
     }
 
-    static class Graph{
+    static class Graph {
         int n;
         ArrayList<Integer>[] adj;
         ArrayList<Integer>[] cost;
         long[] dist;
         PriorityQueue<Entry> queue;
 
-        Graph(int n){
+        Graph(int n) {
             this.n = n;
 
             this.adj = generateAdjacency();
@@ -95,45 +124,68 @@ public class FriendSuggestion {
             this.queue = new PriorityQueue<Entry>(n);
         }
 
-        ArrayList<Integer>[] generateAdjacency(){
-            ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-            for(int i = 0; i < n; i++){
+        ArrayList<Integer>[] generateAdjacency() {
+            ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[n];
+            for (int i = 0; i < n; i++) {
                 adj[i] = new ArrayList<Integer>();
             }
             return adj;
         }
 
-        void addEdge(int x, int y, int c){
+        void addEdge(int x, int y, int c) {
             adj[x].add(y);
             cost[x].add(c);
         }
 
-        void clear(){
+        void clear() {
             Arrays.fill(dist, Long.MAX_VALUE);
             this.queue.clear();
         }
 
         // todo - implement
-        void visit(int i, long d){
+        void visit(int i, long d) {
 
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder s = new StringBuilder();
+
+            for (int i = 0; i < adj.length; i++) {
+                ArrayList<Integer> edges = adj[i];
+                ArrayList<Integer> weights = cost[i];
+                if (i > 0)
+                    s.append("\n");
+                s.append("node " + i + ": ");
+                s.append("adjacencies: " + Arrays.toString(edges.toArray()));
+                s.append("; weights: " + Arrays.toString(weights.toArray()));
+            }
+
+            return s.toString();
         }
     }
 
-
-    static class Entry implements Comparable<Entry>
-    {
+    static class Entry implements Comparable<Entry> {
         Long cost;
         int node;
-      
-        public Entry(Long cost, int node)
-        {
+
+        public Entry(Long cost, int node) {
             this.cost = cost;
             this.node = node;
         }
-     
-        public int compareTo(Entry other)
-        {
+
+        public int compareTo(Entry other) {
             return cost < other.cost ? -1 : cost > other.cost ? 1 : 0;
+        }
+    }
+
+    static class Query {
+        int u;
+        int v;
+
+        @Override
+        public String toString() {
+            return "{u: " + u + ", v: " + v + "}";
         }
     }
 }
