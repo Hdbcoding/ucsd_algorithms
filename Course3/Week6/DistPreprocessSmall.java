1033cdb612543c182387e293aba1db39a04cefc8
henry <henry.byrd@claricode.com>
2020-02-15 12:37:05 -0500

    Setup more boilerplate and a bunch of test cases

--- Course3/Week6/DistPreprocessSmall.java
+++ Course3/Week6/DistPreprocessSmall.java
@@ -2,71 +2,192 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.Random;
 import java.util.Scanner;
 
 public class DistPreprocessSmall {
     public static void main(String[] args) {
-        runSolution();
+        // runSolution();
         testSolution();
     }
 
-    static void testSolution() {
-
+    static void runSolution() {
+        DataScanner in = new StreamScanner();
+        Dijkstra2 a = new Dijkstra2();
+        a.preprocess(in);
+        System.out.println("Ready");
+        respondToQueries(a, in);
+        in.close();
     }
 
-    static void runSolution() {
+    static void testSolution() {
+        // test cases from previous problems
+        runTest(new int[] { 2, 1, 1, 2, 1, 4, 1, 1, 2, 2, 1, 2, 2, 1 }, new long[] { 0, 0, 1, -1 });
+        runTest(new int[] { 4, 4, 1, 2, 1, 4, 1, 2, 2, 3, 2, 1, 3, 6, 1, 1, 3 }, new long[] { 3 });
+        runTest(new int[] { 9, 16, 1, 2, 9, 1, 2, 10, 2, 8, 10, 2, 9, 3, 3, 5, 8, 3, 5, 14, 5, 4, 17, 5, 6, 6, 7, 2, 5,
+                7, 3, 12, 7, 4, 10, 7, 6, 4, 7, 6, 9, 8, 2, 9, 9, 2, 7, 9, 9, 7, 2, 1, 8, 1, 9 },
+                new long[] { 19, 12 });
+        runTest(new int[] { 9, 16, 1, 2, 10, 8, 2, 9, 9, 9, 7, 7, 2, 5, 7, 4, 10, 7, 6, 9, 9, 2, 7, 3, 5, 8, 2, 9, 3, 3,
+                5, 14, 5, 6, 6, 2, 8, 10, 7, 3, 12, 7, 6, 4, 1, 2, 9, 5, 4, 17, 2, 1, 8, 1, 9 }, new long[] { 19, 12 });
+        runTest(new int[] { 4, 6, 1, 2, 200000, 1, 3, 200000, 1, 4, 1000000, 2, 3, 1000000, 2, 4, 200000, 3, 4, 200000,
+                6, 1, 2, 1, 3, 1, 4, 2, 3, 2, 4, 3, 4 },
+                new long[] { 200000, 200000, 400000, 1000000, 200000, 200000 });
+        runTest(new int[] { 8, 5, 7, 6, 6, 1, 4, 9, 3, 5, 6, 1, 2, 7, 3, 8, 15, 2, 1, 4, 3, 8 }, new long[] { 9, 15 });
+        runTest(new int[] { 18, 36, 1, 2, 1838, 1, 3, 793, 2, 4, 1166, 2, 1, 1838, 3, 1, 793, 3, 5, 1009, 4, 6, 1019, 4,
+                2, 1166, 4, 7, 412, 4, 8, 1118, 5, 9, 1627, 5, 3, 1009, 6, 10, 223, 6, 11, 1941, 6, 4, 1019, 6, 12,
+                3220, 7, 4, 412, 7, 13, 1303, 8, 4, 1118, 8, 14, 1897, 9, 15, 479, 9, 16, 806, 9, 5, 1627, 9, 17, 1582,
+                10, 18, 412, 10, 6, 223, 10, 12, 3361, 11, 6, 1941, 12, 10, 3361, 12, 6, 3220, 13, 7, 1303, 14, 8, 1897,
+                15, 9, 479, 16, 9, 806, 17, 9, 1582, 18, 10, 412, 10, 2, 17, 10, 8, 6, 8, 11, 13, 16, 2, 15, 14, 15, 8,
+                6, 5, 7, 1, 17, 11 }, new long[] { 6849, 2360, 2137, 4675, 6073, 9927, 8030, 5825, 3416, 10975 });
+        runTest(new int[] { 5, 20, 1, 2, 667, 1, 3, 677, 1, 4, 700, 1, 5, 622, 2, 1, 118, 2, 3, 325, 2, 4, 784, 2, 5,
+                11, 3, 1, 585, 3, 2, 956, 3, 4, 551, 3, 5, 559, 4, 1, 503, 4, 2, 722, 4, 3, 331, 4, 5, 366, 5, 1, 880,
+                5, 2, 883, 5, 3, 461, 5, 4, 228, 10, 1, 1, 1, 2, 1, 3, 1, 4, 1, 5, 2, 1, 2, 2, 2, 3, 2, 4, 2, 5 },
+                new long[] { 0, 667, 677, 700, 622, 118, 0, 325, 239, 11 });
+        // getting bad distances with multiple edges out of the same node -- fixed
+        runTest(new int[] { 4, 4, 2, 3, 72, 2, 3, 89, 2, 1, 62, 3, 4, 21, 1, 2, 4 }, new long[] { 93 });
+        // getting errors with self-referencing nodes
+        runTest(new int[] { 4, 7, 1, 2, 72, 1, 2, 29, 2, 2, 7, 2, 2, 8, 3, 4, 79, 3, 4, 76, 3, 2, 78, 2, 1, 2, 3, 2 },
+                new long[] { 29, 78 });
+        // some null pointer exception
+        runTest(new int[] { 3, 5, 2, 3, 58, 2, 3, 2, 3, 1, 47, 3, 3, 40, 3, 3, 21, 1, 1, 3 }, new long[] { -1 });
+        // counting matches when a node has been considered but not finalized
+        runTest(new int[] { 7, 13, 1, 1, 83, 1, 2, 13, 1, 2, 78, 1, 5, 63, 2, 5, 4, 3, 2, 10, 3, 7, 1, 4, 5, 98, 5, 3,
+                23, 6, 2, 51, 6, 1, 94, 6, 7, 85, 7, 2, 40, 1, 6, 7 }, new long[] { 79 });
+        // incorrect results in some cases with no-available-path situations
+        runTest(new int[] { 10, 16, 1, 4, 95, 3, 1, 57, 3, 8, 85, 5, 8, 66, 6, 7, 31, 6, 4, 13, 6, 7, 17, 7, 9, 19, 8,
+                6, 61, 8, 1, 89, 8, 1, 4, 9, 9, 98, 9, 4, 60, 9, 6, 47, 9, 10, 89, 10, 7, 46, 4, 6, 1, 7, 1, 9, 1, 10,
+                1 }, new long[] { -1, -1, -1, -1 });
+        // missing path problem
+        runTest(new int[] { 6, 9, 1, 6, 97, 1, 1, 54, 1, 2, 53, 1, 6, 89, 2, 1, 71, 3, 4, 13, 5, 3, 60, 5, 2, 85, 6, 1,
+                9, 1, 5, 6 }, new long[] { 245 });
+        // missing path problem
+        runTest(new int[] { 8, 10, 1, 5, 2, 1, 7, 77, 2, 7, 98, 2, 3, 29, 3, 6, 21, 4, 6, 18, 5, 8, 19, 6, 8, 58, 7, 3,
+                64, 7, 2, 24, 1, 7, 8 }, new long[] { 132 });
+        // missing path
+        runTest(new int[] { 7, 13, 1, 6, 68, 1, 2, 19, 2, 1, 76, 2, 1, 10, 4, 2, 20, 4, 5, 88, 4, 3, 24, 4, 3, 40, 4, 1,
+                13, 5, 7, 20, 5, 6, 1, 6, 4, 64, 7, 2, 94, 1, 2, 7 }, new long[] { 250 });
 
+        int maxNumNodes = 100;
+        int maxWidth = 1000000;
+        int numTests = 10000;
+        stressTest(maxNumNodes, maxWidth, numTests);
+        runComparisons(maxNumNodes, maxWidth, numTests);
+    }
+
+    static void runComparisons(int maxNumNodes, int maxWidth, int numTests) {
+        int i = 15;
+        while (i-- > 0) {
+            int seed = (int) System.currentTimeMillis();
+            stressCompare(seed, maxNumNodes, maxWidth, numTests, Dijkstra.class);
+            stressCompare(seed, maxNumNodes, maxWidth, numTests, Dijkstra2.class);
+        }
+    }
+
+    static <T extends GraphSolver> void stressCompare(int seed, int maxNumNodes, int maxGraphWidth, int numTests,
+            Class<T> type) {
+        Random r = new Random(seed);
+        long startTime = System.currentTimeMillis();
+        GraphSolver solver = new Instantiator<T>().getInstance(type);
+        for (int i = 0; i < numTests; i++) {
+            System.out.print(type.getName() + "elapsed: %" + (double) 100 * i / numTests + "\r");
+            int[] data = generateData(maxNumNodes, maxGraphWidth, r);
+            solver.preprocess(new ArrayScanner(data));
+
+            int queries = nextInt(data[0], r);
+            for (int j = 0; j < queries; j++) {
+                int u = nextInt(data[0], r);
+                int v = nextInt(data[0], r);
+                try {
+                    solver.distance(u, v);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(type.getName() + " total time (ms): " + (endTime - startTime));
+    }
+
+    static void runTest(int[] data, long[] expected) {
+        DataScanner in = new ArrayScanner(data);
+        Graph g = parseGraph(in);
+        TwoWayGraph g2 = TwoWayGraph.fromGraph(g);
+        Dijkstra d = new Dijkstra();
+        d.preprocess(g);
+        Dijkstra2 d2 = new Dijkstra2();
+        d2.preprocess(g2);
+        Query[] queries = parseQueries(in);
+        String expectedString = Arrays.toString(expected);
+        boolean allTechniquesWork = evaluate(d, queries, expectedString);
+        allTechniquesWork &= evaluate(d2, queries, expectedString);
+        if (!allTechniquesWork) {
+            System.out.println("Queries: " + Arrays.toString(queries));
+            System.out.println("Graph: " + g);
+        }
+    }
+
+    static boolean evaluate(GraphSolver solver, Query[] queries, String expectedString) {
+        long[] actual = respondToQueries(solver, queries);
+        String actualString = Arrays.toString(actual);
+        boolean worked = actualString.equals(expectedString);
+        if (!worked) {
+            System.out.println("Unexpected result for technique " + solver.getClass().getName());
+            System.out.println("Expected: " + expectedString + ", but got " + actualString);
+        }
+        return worked;
+    }
+
+    static void stressTest(int maxNumNodes, int maxGraphWidth, int numTests) {
+        Random r = new Random();
+        Dijkstra d = new Dijkstra();
+        Dijkstra2 d2 = new Dijkstra2();
+
+        for (int i = 0; i < numTests; i++) {
+            System.out.print("Stress tests: %" + (double) 100 * i / numTests + "\r");
+            int[] data = generateData(maxNumNodes, maxGraphWidth, r);
+            int n = data[0];
+            Graph g = parseGraph(data);
+            TwoWayGraph g2 = TwoWayGraph.fromGraph(g);
+
+            d.preprocess(g);
+            d2.preprocess(g2);
+
+            boolean anyMistakes = false;
+            int queries = nextInt(n, r);
+            for (int j = 0; j < queries; j++) {
+                int u = nextInt(n, r);
+                int v = nextInt(n, r);
+                long expected = d.distance(u, v);
+                try {
+                    long actual_d2 = d2.distance(u, v);
+                    if (expected != actual_d2) {
+                        anyMistakes = true;
+                        System.out.println("\n\n\n\nUnexpected distance during test run " + i);
+                        System.out.println("for nodes " + u + " to " + v);
+                        System.out.println("expected " + expected);
+                        System.out.println("dijkstra2 " + actual_d2);
+                    }
+                } catch (Exception e) {
+                    anyMistakes = true;
+                    System.out.println("\n\n\n\nError thrown during test run " + i);
+                    System.out.println("for nodes " + u + " to " + v);
+                    System.out.println("expected " + expected);
+                    e.printStackTrace();
+                }
+            }
+
+            if (anyMistakes) {
+                System.out.println("\nn: " + data[0] + ", m: " + data[1]);
+                System.out.println(Arrays.toString(data));
+                System.out.println(g);
+            }
+        }
     }
 
     public interface GraphSolver {
         long distance(int u, int v);
 
         void preprocess(DataScanner in);
-    }
-
-    static class FloydWarshall implements GraphSolver {
-        long[][] distances;
-        Graph g;
-
-        public void preprocess(DataScanner in) {
-            preprocess(parseGraph(in));
-        }
-
-        void preprocess(Graph g) {
-            distances = new long[g.s][g.s];
-
-            for (int i = 0; i < g.s; i++) {
-                Arrays.fill(distances[i], -1);
-                ArrayList<Integer> adj = g.adj[i];
-                ArrayList<Integer> cst = g.cost[i];
-                for (int j = 0; j < adj.size(); j++) {
-                    int n = adj.get(j);
-                    int w = cst.get(j);
-                    long old = distances[i][n];
-                    distances[i][n] = old == -1 || w < old ? w : old;
-                }
-                distances[i][i] = 0;
-            }
-
-            for (int k = 0; k < g.s; k++) {
-                for (int i = 0; i < g.s; i++) {
-                    for (int j = 0; j < g.s; j++) {
-                        long oldD = distances[i][j];
-                        long ik = distances[i][k];
-                        long kj = distances[k][j];
-
-                        if (ik == -1 || kj == -1)
-                            continue;
-                        if (oldD == -1 || oldD > ik + kj)
-                            distances[i][j] = ik + kj;
-                    }
-                }
-            }
-        }
-
-        public long distance(int s, int t) {
-            return distances[s][t];
-        }
     }
 
     static class Dijkstra implements GraphSolver {
@@ -214,6 +335,11 @@ public class DistPreprocessSmall {
         }
     }
 
+    static Graph parseGraph(int[] data) {
+        ArrayScanner in = new ArrayScanner(data);
+        return parseGraph(in);
+    }
+
     static Graph parseGraph(DataScanner in) {
         int n = in.nextInt();
         int m = in.nextInt();
@@ -240,6 +366,61 @@ public class DistPreprocessSmall {
             g.addEdge(x - 1, y - 1, c);
         }
         return g;
+    }
+
+    static Query[] parseQueries(DataScanner in) {
+        int t = in.nextInt();
+        Query[] queries = new Query[t];
+        int u, v;
+        for (int i = 0; i < t; i++) {
+            u = in.nextInt();
+            v = in.nextInt();
+            queries[i] = new Query(u - 1, v - 1);
+        }
+        return queries;
+    }
+
+    static void respondToQueries(GraphSolver solver, DataScanner in) {
+        int t = in.nextInt();
+        int u, v;
+        for (int i = 0; i < t; i++) {
+            u = in.nextInt();
+            v = in.nextInt();
+            System.out.println(solver.distance(u - 1, v - 1));
+        }
+    }
+
+    static long[] respondToQueries(GraphSolver solver, Query[] queries) {
+        long[] results = new long[queries.length];
+        Query q;
+        for (int i = 0; i < queries.length; i++) {
+            q = queries[i];
+            results[i] = solver.distance(q.u, q.v);
+        }
+        return results;
+    }
+
+    static int[] generateData(int maxNumNodes, int maxGraphWidth, Random r) {
+        int n = nextInt(maxNumNodes, r) + 1;
+        int m = nextInt(n * n, r);
+        return fillEdges(n, m, maxGraphWidth, r);
+    }
+
+    static int[] fillEdges(int n, int m, int maxGraphWidth, Random r) {
+        int[] data = new int[n * 2 + m * 3 + 2];
+        data[0] = n;
+        data[1] = m;
+        for (int i = 0; i < m; i++) {
+            int j = i * 3 + 2;
+            data[j] = nextInt(n, r) + 1;
+            data[j + 1] = nextInt(n, r) + 1;
+            data[j + 2] = nextInt(maxGraphWidth, r) + 1;
+        }
+        return data;
+    }
+
+    static int nextInt(int bound, Random r) {
+        return Math.abs(r.nextInt(bound));
     }
 
     static class Graph {
@@ -362,6 +543,21 @@ public class DistPreprocessSmall {
         @Override
         public int compareTo(Node other) {
             return distance < other.distance ? -1 : distance > other.distance ? 1 : 0;
+        }
+    }
+
+    static class Query {
+        int u;
+        int v;
+
+        Query(int u, int v) {
+            this.u = u;
+            this.v = v;
+        }
+
+        @Override
+        public String toString() {
+            return "{ u: " + u + ", v: " + v + " }";
         }
     }
 
